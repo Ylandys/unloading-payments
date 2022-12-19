28f686137d018431811fb7c14f1549358726ed89
Ylandys <goldwolf59112@mail.ru>
2022-12-19 11:08:28 +0700

    Главная форма платежей дизайн - ГОТОВ

---
+++ WorkWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Registr
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWindow.xaml
+    /// </summary>
+    public partial class WorkWindow : Window
+    {
+        public WorkWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
