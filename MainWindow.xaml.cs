e89f39c0d349024ed31db248cd7e3bdaf7eb04c7
Ylandys <goldwolf59112@mail.ru>
2022-12-19 09:45:06 +0700

    Создана форма авторизации.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Registr
         {
             InitializeComponent();
         }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
