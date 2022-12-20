662259e9a840b2de61187b51fa3b7cd183760afd
Ylandys <goldwolf59112@mail.ru>
2022-12-20 21:44:57 +0700

    Выгрузка is done

--- Windows/WorkWindow.xaml.cs
+++ Windows/WorkWindow.xaml.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 //using
-using OfficeOpenXml;
+using Syncfusion.UI.Xaml.Grid.Converter;
+using Microsoft.Win32;
+using System.Windows.Input;
+using ClosedXML.Excel;
+using System.Diagnostics;
 using System.IO;
 
 namespace Registr
@@ -30,7 +25,25 @@ namespace Registr
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ExportToExcel();
+        }
 
+        private void ExportToExcel()
+        {
+            this.TablePayments.SelectAllCells();
+            this.TablePayments.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+            ApplicationCommands.Copy.Execute(null, this.TablePayments);
+            this.TablePayments.UnselectAllCells();
+            String result = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+            try
+            {
+                StreamWriter sw = new StreamWriter("wpfdata.csv");
+                sw.WriteLine(result);
+                sw.Close();
+                Process.Start("wpfdata.csv");
+            }
+            catch (Exception ex)
+            { }
         }
 
 
@@ -53,7 +66,7 @@ namespace Registr
         {
             refreshdatagrid();
         }
-
+        
         private void Receive_Payments_Click(object sender, RoutedEventArgs e)
         {
             TablePayments.ItemsSource = database.Payments.ToList();
@@ -62,7 +75,7 @@ namespace Registr
             {
                 string formatted = selectedDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
                 //Фильтр календарь
-            TablePayments.ItemsSource = database.Payments.Where(d => formatted == d.DateOfAdmission.ToString()).ToList();
+                TablePayments.ItemsSource = database.Payments.Where(d => formatted == d.DateOfAdmission.ToString()).ToList();
             }
 
         }
@@ -76,6 +89,20 @@ namespace Registr
             else
             {
                 TablePayments.ItemsSource = database.Payments.ToList();
+            }
+        }
+
+        private void Open_Folder_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            {
+                string dn = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\Платежи";
+
+                openFile.InitialDirectory = dn;
+
+                openFile.ShowDialog();
+
+                PathToFile_txt.Text = dn;
             }
         }
     }
